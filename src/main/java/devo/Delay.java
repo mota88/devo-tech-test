254ef9facdefb1cf3575d5fc1f3ed6f65aa60043
mota88 <mota88@users.noreply.github.com>
2021-02-15 08:36:29 +0100

    Comments in Delay class

--- src/main/java/devo/Delay.java
+++ src/main/java/devo/Delay.java
@@ -4,30 +4,37 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
-// java.util.Timer In some simple use cases you may just want to wait a bit and continue execution within the same thread/flow.
-
+/*
+ * Implements a delay in the execution of the current thread/flow. This serves the purpose
+ * of our program, in which we just need to set a period of time after which the screen is
+ * updated. In order to do that, unlike other possible solutions, it seems simpler to stop
+ * the execution of the current thread, instead of creating new threads. 
+ */
 public class Delay {
     /** 
-    *  Delays the current thread execution. 
-    *  The thread loses ownership of any monitors. 
-    *  Quits immediately if the thread is interrupted
     *  
-    * @param duration the time duration in milliseconds
+    * @param duration	Long that contains the duration in milliseconds
     */
    public void delay(final long durationInMillis) {
       delay(durationInMillis, TimeUnit.MILLISECONDS);
    }
 
    /** 
-    * @param duration the time duration in the given {@code sourceUnit}
-    * @param unit
+    * Delays the current thread execution and quits
+    * immediately if the thread is interrupted
+    * 
+    * @param duration	Long that contains the time duration
+    * @param unit		TimeUnit that contains the unit for time - always 
+    * 					receives SECONDS, but can be modified  
     */
     public void delay(final long duration, final TimeUnit unit) {
         long currentTime = System.currentTimeMillis();
-        long deadline = currentTime+unit.toMillis(duration);
+        long deadline = currentTime + unit.toMillis(duration);
+        // thread synchronization mechanism
         ReentrantLock lock = new ReentrantLock();
+        // provides the ability for the thread to wait for some condition to occur
         Condition waitCondition = lock.newCondition();
-
+        // lock the thread for the given time and unlocks it after that  
         while ((deadline-currentTime)>0) {
             try {
                 lock.lockInterruptibly();    
